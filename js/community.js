f34287d75d2f4c2bb3e934f19e929e2b76b5d2f2
Lee-sojung <thwjd3698@gmail.com>
2022-03-15 01:24:49 +0900

    커뮤니티 스크립트 연결 및 반응형 완성

--- js/community.js
+++ js/community.js
@@ -1,15 +1,53 @@
+//탭버튼 기능 
+const btnsTab = document.querySelectorAll('#community #tabbtns li');
+const boxsTab = document.querySelectorAll('#community .container>div');
 
-//변수 저장 
-const btns = document.querySelectorAll(".panel-heading"); 
+btnsTab.forEach((btn, idx)=>{
+  btn.addEventListener('click', e=>{
+    e.preventDefault();
+
+    for(const btn of btnsTab) btn.classList.remove('on');
+    for(const box of boxsTab) box.classList.remove('on');
+
+    btnsTab[idx].classList.add('on');
+    boxsTab[idx].classList.add('on');
+  })  
+})
 
 
+//faq toggle
+const toggleWrap = document.querySelectorAll('.panel-question');
+
+toggleWrap.forEach((item, idx)=>{
+  const btn = item.querySelector('.panel-heading');
+
+  btn.addEventListener('click', e =>{
+    for(const toggled of toggleWrap) toggled.classList.remove('on');
+    toggleWrap[idx].classList.add('on');
+  })
+})
 
 
-//이벤트바인딩 
-//btnCall을 클릭할 때 
-btns.onclick = function(e){
-    //링크이동 금지 
-  e.preventDefault(); 
-  //btnCall에 on이 있으면 제거, 없으면 추가 
-    btns.classList.toggle("on"); 
+//countdown
+const countDays = '1 may 2022';
+function countdown(){
+  const conutDaysDate = new Date(countDays);
+  const currentDate = new Date();
+
+  const totalSeconds = (conutDaysDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  document.getElementById('days').innerText = formatTime(days);
+  document.getElementById('hours').innerText = formatTime(hours);
+  document.getElementById('minutes').innerText = formatTime(minutes);
+  document.getElementById('seconds').innerText = formatTime(seconds);
 }
+
+function formatTime(time){
+  return time <10 ? '0' + time : time;
+}
+
+setInterval(countdown, 1000);
